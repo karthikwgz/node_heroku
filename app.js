562c9862758156d7ab44683e1f753d4270024886
karthikwgz <abhinavkarthik.rakendu@gmail.com>
2021-08-06 12:05:00 +0530

    new

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get("/",(req,res) =>{
 	res.send("hello");
 })
 
-app.listen(process.env.PORT || 3000)
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+	console.log(`Server running at ${PORT}`);
+})
